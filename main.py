df03172956192053c8089e843035f47daa762326
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-04-07 04:49:09 +0700

    Recode everything

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import time
 import datetime as dt
 
 # Slow print code
-def print_slow(str):
-    for letter in str:
+def slow_print(text):
+    for letter in text:
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(0.05)
@@ -14,35 +14,35 @@ def print_slow(str):
 
 # Main Menu
 
-print_slow("Simple Captcha Authentication\n")
-print_slow("Made With Python BY LyQuid")
+slow_print("Simple Captcha Authentication\n")
+slow_print("Made With Python BY LyQuid")
 print("")
 
-# Generate Random 2FA
+# Generate Random Captcha
 key_code = "".join(random.choices(
             string.ascii_uppercase + string.digits + string.ascii_lowercase,
             k = 7
 ))
 
-# 2FA
+# Captcha
 print("\nCaptcha :")
 print("=========")
-print_slow(f"{key_code}\n")
+slow_print(f"{key_code}\n")
 print("=========\n")
 
-print_slow("Captcha Code: ")
+slow_print("Captcha Code: ")
 t = dt.datetime.now()
 key_answer = input("")
 
 time_taken = dt.datetime.now ()-t
 
-# 2FA Results
+# Captcha Results
 
-if(key_answer == key_code):
-  print_slow("\nNice, Captcha code matches\n")
-  print(f"Time Taken : {time_taken}")
-  print("")
+if key_answer == key_code:
+	slow_print("\nNice, Captcha code matches\n")
+	print(f"Time Taken : {time_taken}")
+	print("")
 
 else:
-  print_slow("\nBruh, Captcha code does not match\n")
-  print(f"Time Taken : {time_taken}")
+	slow_print("\nBruh, Captcha code does not match\n")
+	print(f"Time Taken : {time_taken}")
